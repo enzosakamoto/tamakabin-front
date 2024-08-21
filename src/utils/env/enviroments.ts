f02e464764a4942507adcb9b2f8e59cda4e84a6c
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-08-21 18:52:57 -0300

    fix: fix env

--- src/utils/env/enviroments.ts
+++ src/utils/env/enviroments.ts
@@ -1,5 +1,5 @@
 export const enviroments = {
   mongodbUri: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URI}`,
-  database: process.env.MONGODB_DATABASE,
-  collection: process.env.MONGODB_COLLECTION
+  database: process.env.MONGODB_DATABASE || '',
+  collection: process.env.MONGODB_COLLECTION || ''
 }
